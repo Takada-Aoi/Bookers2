16db1de393223bf7dc9599bd40c909974fc14cc0
Takada-Aoi <a.m.3m0221@gmail.com>
2021-04-11 13:53:48 +0000

    [Add]3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  devise_for :users
  root to: 'homes#top'
+ get "about" =>"homes#about",as:"about"
  resources :users,only:[:index,:show,:edit,:update]
+ resources :books,only:[:index,:show,:create,:edit,:destroy]
 end
