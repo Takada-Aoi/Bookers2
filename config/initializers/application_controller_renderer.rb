b9cc4f03f275d333e2a2d3a79819c277c3570130
Takada-Aoi <a.m.3m0221@gmail.com>
2021-04-11 08:25:30 +0000

    [Add]2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '774be4de788b0f337d4cef798624fbd4932a27022208ab5e4b559113315ca29b40563730699f6b309fa20573533ce8c1d46e92fac1a3ccdff28a25a8b21f7d66'
